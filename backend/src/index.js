2f61885452bc2cd15e54fcab5fe64a0c091c81a2
Krishnapriya M A <kkpkpkp777@gmail.com>
2025-07-13 15:32:38 +0530

    fix: move dotenv.config() to top for correct env loading

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,7 @@ import messageRoutes from "./routes/message.routes.js";
 import cors from "cors";
 import {app,server} from "./lib/socket.js";
 import path from "path";
+dotenv.config();
 app.use(cookieParser());
 app.use(express.json({limit:'10mb'}));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
@@ -14,7 +15,6 @@ app.use(cors({
     origin:"http://localhost:5173",
     credentials:true, 
 }))
-dotenv.config();
 const port = process.env.PORT
 const __dirname=path.resolve();
 app.use("/api/auth", authRoutes);
@@ -27,6 +27,7 @@ if(process.env.NODE_ENV==="production")
     res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
 })
 }
+
 server.listen(port, () => {
     console.log("Server is running on port: ", +port);
     connectdb();
